529b0388e19cf192bdf41bd938fbb96f4759eee0
Petch Settapak <srtp.th@gmail.com>
2022-10-17 00:05:14 +0700

    edit logo loading page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Home from "@/components/component-pages/Home";
 import Layout from "@/components/layouts/Layout";
-import { Image, useMediaQuery } from "@chakra-ui/react";
+import { Grid, GridItem, useMediaQuery } from "@chakra-ui/react";
+import Image from "next/image";
 import { ReactElement, useEffect, useState } from "react";
 
 import type { NextPageWithLayout } from "./_app";
@@ -20,16 +21,16 @@ const Page: NextPageWithLayout = () => {
 
   return (
     <>
-      {loading && (isMaxWidth767 || isMinToMAX) ? (
-        <div className="w-screen h-screen  z-50 bg-[#D52D2C] top-0 left-0 bottom-0 right-0 fixed sticky-0 overflow-y-hidden">
-          <Image
-            src="images/logo.webp"
-            display="block"
-            mt="300px"
-            mx="auto"
-            width="50%"
-            alt="logo"
-          />
+      {isMaxWidth767 || isMinToMAX ? (
+        <div className="w-screen h-screen grid grid-cols-3  grid-rows-5 z-50 bg-[#D52D2C] top-0 left-0 bottom-0 right-0 fixed sticky-0 overflow-y-hidden">
+          <div className="col-start-2 row-start-3  block mx-auto ">
+            <Image
+              src={"/images/logo.webp"}
+              height={"200px"}
+              width={"200px"}
+              alt="logo"
+            />
+          </div>
         </div>
       ) : (
         <Home />
