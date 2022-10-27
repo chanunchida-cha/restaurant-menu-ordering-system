32344a689ed3ad7d90e1c941fdce81e96beb190d
Yok <yok.8.12.2543@gmail.com>
2022-10-27 22:12:20 +0700

    test

--- components/component-pages/Home.tsx
+++ components/component-pages/Home.tsx
@@ -90,35 +90,14 @@ function Home() {
 
   return (
     <div className=" z-20">
-      <InputGroup>
-        <InputLeftElement
-          pointerEvents="none"
-          children={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          }
-        ></InputLeftElement>
-        <Input
-          placeholder="ค้นหาเมนูอาหาร"
-          focusBorderColor="#EC9191"
-          value={searchText}
-          onChange={(e) => {
-            setSearchText(e.target.value);
-          }}
-        />
-      </InputGroup>
+      <Input
+        placeholder="ค้นหาเมนูอาหาร"
+        focusBorderColor="#EC9191"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+      />
 
       {isMaxWidth767 || isMinToMAX ? (
         <div className=" w-screen h-[100px] z-30  grid grid-cols-4  grid-rows-1 fixed bottom-0 sticky-0 left-0 ">
